45a46b0e6ffc063cc8fb9a3de7ba1847e7dfc86b
Cookkie92 <74307133+Cookkie92@users.noreply.github.com>
2021-10-06 20:47:32 +0200

    jas

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 console.log("yeet");
+const PORT = 5500;
+const axios = require("axios");
+const cheerio = require("cheerio");
+const express = require("express");
+
+const app = express();
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
